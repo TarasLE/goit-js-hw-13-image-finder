304975f8b228e0222c6793c8f15f9df1e0286aff
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-21 15:30:24 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 import './css/styles.css';
+import ApiServices from './js/components/apiService'
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
-    articlesContainer: document.querySelector('.gallery')
+    imageContainer: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]')
 }
 
+const apiServices = new ApiServices();
+
+let searhQuery = '';
+
 refs.searchForm.addEventListener('submit', onSearch)
+refs.loadMoreBtn.addEventListener('click', onLoadMore)
 
 function onSearch(event) {
     event.preventDefault();
 
     const searhQuery = event.currentTarget.elements.query.value;
 
-    const option = {
-    headers: {
-        // Authorization: '19125806-9a56a48a4edb0ea3b4b1e3bdb'
-        }
-    }
-
-const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searhQuery}&page=1&per_page=12&key=19125806-9a56a48a4edb0ea3b4b1e3bdb`
-    // const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=19125806-9a56a48a4edb0ea3b4b1e3bdb/${searchQuery}`
-    fetch(url, option).then(response => response.json()).then(console.log())
- }
+    apiServices.fetchImages(searhQuery)
+}
+ 
+function onLoadMore() {
+    apiServices.fetchImages(searhQuery)
+}
 
 
 
---
+++ src/js/components/apiService.js
@@ -0,0 +1,15 @@
+export default class ApiService {
+    constructor(){
+    
+    }
+
+    fetchImages() {
+        const option = {
+        headers: {}
+    }
+
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searhQuery}&page=1&per_page=12&key=19125806-9a56a48a4edb0ea3b4b1e3bdb`
+  
+    fetch(url, option).then(response => response.json()).then(console.log())
+    }
+}
