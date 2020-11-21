3d8748a68370758e26a5f3375dee2ceb94bd7fe8
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-21 14:47:27 +0200

    Updates

---
+++ main.5d926046cf966602f395.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{OPH6:function(e,a,o){},QfWi:function(e,a,o){"use strict";o.r(a);o("JBxO"),o("FdtR"),o("OPH6");fetch("https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=cat&page=1&per_page=12&key=19125806-9a56a48a4edb0ea3b4b1e3bdb",{headers:{Authorization:"19125806-9a56a48a4edb0ea3b4b1e3bdb"}}).then((function(e){return e.json()})).then(console.log())}},[["QfWi",1,2]]]);
+//# sourceMappingURL=main.5d926046cf966602f395.js.map
