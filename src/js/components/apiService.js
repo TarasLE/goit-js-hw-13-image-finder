9b3e6d2dab3318705130a149e9671ab3f77c5ae0
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-21 15:57:40 +0200

    interim save

--- src/js/components/apiService.js
+++ src/js/components/apiService.js
@@ -1,6 +1,7 @@
 export default class ApiService {
     constructor() {
         this.searhQuery = '';
+        this.page = 1;
     }
 
     fetchImages(searhQuery) {
@@ -8,9 +9,9 @@ export default class ApiService {
         headers: {}
     }
 
-    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searhQuery}&page=1&per_page=12&key=19125806-9a56a48a4edb0ea3b4b1e3bdb`
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searhQuery}&page=${this.page}&per_page=12&key=19125806-9a56a48a4edb0ea3b4b1e3bdb`
   
-    fetch(url, option).then(response => response.json()).then(console.log())
+        fetch(url, option).then(response => response.json()).then(data => { this.page += 1; });
     }
 
     get query(){
